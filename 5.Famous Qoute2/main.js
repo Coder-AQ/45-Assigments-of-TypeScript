fa2ede7b492b06c42080f01c926f7a0bc7e759d9
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-16 16:52:04 +0500

    Add files via upload

---
+++ 5.Famous Qoute2/main.js
@@ -0,0 +1,4 @@
+var Famous_Person = "Albert Einstine";
+var Quote = "A person who never made made mistake will never tried new things";
+var message = "".concat(Famous_Person, " once said, \"").concat(Quote, "\"");
+console.log(message);
---
+++ 5.Famous Qoute2/main.ts
@@ -0,0 +1,7 @@
+let Famous_Person : string = "Albert Einstine";
+
+let Quote : string="A person who never made made mistake will never tried new things";
+
+let message :string = `${Famous_Person} once said, "${Quote}"`;
+
+console.log(message)
