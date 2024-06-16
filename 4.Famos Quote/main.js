9b8aebb1873689341e29576b78ac496912de54ab
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-16 16:50:06 +0500

    Add files via upload

---
+++ 4.Famos Quote/main.js
@@ -0,0 +1,3 @@
+var quote = "A person who never made a mistake never tried anything new.";
+var Aurthor = "Alber Einstine";
+console.log("".concat(Aurthor, " once said: ").concat(quote, " "));
---
+++ 4.Famos Quote/main.ts
@@ -0,0 +1,4 @@
+let quote : string = "A person who never made a mistake never tried anything new.";
+let Aurthor = "Alber Einstine"
+
+console.log(`${Aurthor} once said: ${quote} `)
