76f4922c01a8fcc2a828a12715ac905d1da621e1
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-16 16:54:03 +0500

    Delete Name_Cases directory

--- Name_Cases/main.ts
+++
@@ -1,7 +0,0 @@
-let Person_Name :string ="fahad"
-
-console.log("Lower Case", Person_Name.toLowerCase())
-
-console.log("Upper Case",Person_Name.toUpperCase())
-
-console.log("Title Case",Person_Name.replace(/\b\w/g,c=> c.toUpperCase()))
