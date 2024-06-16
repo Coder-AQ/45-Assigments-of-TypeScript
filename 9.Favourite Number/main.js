ca0ff6d2efa22a6aae9491d21bceec7940ac8e68
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-16 16:58:52 +0500

    Add files via upload

---
+++ 9.Favourite Number/main.js
@@ -0,0 +1,2 @@
+var favouritenumber = 5;
+console.log("My favourite number is ".concat(favouritenumber, "."));
---
+++ 9.Favourite Number/main.ts
@@ -0,0 +1,2 @@
+let favouritenumber : number =5 ;
+console.log(`My favourite number is ${favouritenumber}.`);
