0cd8dcc908bc1f0dc21b696c2c194c0c08a517bc
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-16 17:00:23 +0500

    Add files via upload

---
+++ 10.Adding Comments/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//NAME:Abdurehman
+//Date: 15-3-24
+//favorite nmber 
+//revealed my faborite number in the program
---
+++ 10.Adding Comments/main.ts
@@ -0,0 +1,6 @@
+//NAME:Abdurehman
+//Date: 15-3-24
+
+//favorite nmber 
+
+//revealed my faborite number in the program
