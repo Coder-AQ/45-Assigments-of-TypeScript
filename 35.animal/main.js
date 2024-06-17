ef435ba366490862b05020f54c5e65dd8403c486
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:08:02 +0500

    Add files via upload

---
+++ 35.animal/main.js
@@ -0,0 +1,6 @@
+var animals = ["dog", "cat", "rabbit"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("A ".concat(animal, " would make a great pet."));
+}
+console.log("Any of these animals would make a great pet!");
---
+++ 35.animal/main.ts
@@ -0,0 +1,10 @@
+
+let animals: string[] = ["dog", "cat", "rabbit"];
+
+
+for (let animal of animals) {
+  console.log(`A ${animal} would make a great pet.`);
+}
+
+
+console.log("Any of these animals would make a great pet!");
