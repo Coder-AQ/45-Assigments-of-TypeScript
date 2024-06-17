5944a1b1d94d1b2d65dd66c187599e70a904b1b8
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:56:31 +0500

    Add files via upload

---
+++ 26.alien colour 2/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let aline_color = "green";
+if (aline_color == "green") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else
+    (console.log("The player just earned 10 points"));
+aline_color = "Red";
+if (aline_color == "green") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("The player just earned 10 points by shooting and red alien");
+}
