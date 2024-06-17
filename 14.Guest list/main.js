d7d4f0518a5fba161ba4ebb932d494bafc5c9ac0
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:38:52 +0500

    Add files via upload

---
+++ 14.Guest list/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+let guest_list = ['aazain', 'hadi', 'rafay', 'rashid'];
+exports.guest_list = guest_list;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`we invite u on dinner tomorrow ${guest_list[i]}`);
+}
---
+++ 14.Guest list/main.ts
@@ -0,0 +1,7 @@
+let guest_list : string [] = ['aazain','hadi','rafay','rashid'];
+
+for(let i=0; i<guest_list.length; i++){
+
+console.log(`we invite u on dinner tomorrow ${guest_list[i]}`)
+}
+export{guest_list}
