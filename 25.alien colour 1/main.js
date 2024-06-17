d754a6ddd9ce310ad5b80ffeb111f93f8efae15a
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:56:00 +0500

    Add files via upload

---
+++ 25.alien colour 1/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let alienColour = "green";
+if (alienColour == "green") {
+    console.log("Ther player just earned 5 points");
+}
+alienColour = "Red";
+if (alienColour == "green") {
+    console.log("Ther player just earned 5 points");
+}
---
+++ 25.alien colour 1/main.ts
@@ -0,0 +1,11 @@
+let alienColour :string ="green"
+
+if (alienColour == "green") {
+    console.log("Ther player just earned 5 points")
+}
+
+alienColour = "Red"
+
+if (alienColour == "green") {
+    console.log("Ther player just earned 5 points")
+}
