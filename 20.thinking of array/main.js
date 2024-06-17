5eb7ca37018c9f50f3802a20d41e83bd8947329e
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:49:49 +0500

    Add files via upload

---
+++ 20.thinking of array/main.js
@@ -0,0 +1,6 @@
+var cars = ['civic', 'toyota', 'nissan', 'hyundai', 'suzuki'];
+console.log("list of cars ");
+for (var _i = 0, cars_1 = cars; _i < cars_1.length; _i++) {
+    var top_1 = cars_1[_i];
+    console.log(top_1);
+}
---
+++ 20.thinking of array/main.ts
@@ -0,0 +1,3 @@
+let cars : string [] = ['civic','toyota','nissan','hyundai','suzuki']
+console.log(`list of cars:- `)
+for(let top of cars){console.log(top)}
