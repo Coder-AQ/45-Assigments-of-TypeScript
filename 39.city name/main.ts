68b4e96ef9a9610b29f275cb48e630a4a9e17773
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:10:18 +0500

    Add files via upload

---
+++ 39.city name/main.ts
@@ -0,0 +1,8 @@
+function city_names(city: string, country: string): string {
+    return `${city}, ${country}`;
+  }
+  
+  // Result
+  console.log(city_names("London", "USA"));
+  console.log(city_names("delhi", "india"));
+  console.log(city_names("quetta", "pakistan"));
