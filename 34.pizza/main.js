1b1b853c61d6788343b938863202031db366161f
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:08:00 +0500

    Add files via upload

---
+++ 34.pizza/main.js
@@ -0,0 +1,8 @@
+var Favourite_Pizzas = ["MargaritaPizza", "Fajita", "SpicyArabian"];
+for (var i = 0; i < Favourite_Pizzas.length; i++) {
+    console.log(Favourite_Pizzas[i]);
+}
+for (var i = 0; i < Favourite_Pizzas.length; i++) {
+    console.log("I like to eat ".concat(Favourite_Pizzas));
+}
+console.log("I really like to eat pizzas becasue they come in diffrent flavours");
---
+++ 34.pizza/main.ts
@@ -0,0 +1,10 @@
+let Favourite_Pizzas : string [] = ["MargaritaPizza","Fajita","SpicyArabian"]
+
+for ( let i =0 ; i < Favourite_Pizzas.length ; i++  ) {
+
+    console.log(Favourite_Pizzas[i])
+}
+for (let i =0 ; i < Favourite_Pizzas.length ; i++) {
+    console.log(`I like to eat ${ Favourite_Pizzas}`)
+}
+console.log("I really like to eat pizzas becasue they come in diffrent flavours")
