bf2330b236fc3ad187125a563020f3b22330a17d
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:05:02 +0500

    Add files via upload

---
+++ 31.no users/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let users = ["Admin", "Ayesha", "Rafay", "Rashid", "Suleman"];
+if (users.length === 0) {
+    console.log("We need to find some users");
+}
+else {
+    users = [];
+    console.log("All users have be removed " + users.length);
+}
---
+++ 31.no users/main.ts
@@ -0,0 +1,9 @@
+let users : string [] = ["Admin","Ayesha","Rafay","Rashid","Suleman"]
+
+if (users.length === 0) {
+    console.log("We need to find some users")
+}
+else {
+    users = [];
+    console.log("All users have be removed " + users.length)
+}
