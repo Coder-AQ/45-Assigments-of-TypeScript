68bf3fb97c767c10ced0ae6cfb07bc24fb8fba1a
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:59:47 +0500

    Add files via upload

---
+++ 29.favourite fruit/main.js
@@ -0,0 +1,16 @@
+var Favourite_Fruit = ["Apple", "Banana", "Pineapple"];
+if (Favourite_Fruit.includes("Apple")) {
+    console.log("I really like Apple");
+}
+if (Favourite_Fruit.includes("Banana")) {
+    console.log("I really like Banana");
+}
+if (Favourite_Fruit.includes("Pineapple")) {
+    console.log("I really like Pineapple");
+}
+if (Favourite_Fruit.includes("Grapes")) {
+    console.log("I really like Grapes");
+}
+if (Favourite_Fruit.includes("Mango")) {
+    console.log("I really like Mangos");
+}
