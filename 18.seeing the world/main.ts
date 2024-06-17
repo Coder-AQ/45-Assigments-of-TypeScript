c463596c338cee8e261fd4be469e27fc4e36c88c
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:44:50 +0500

    Add files via upload

---
+++ 18.seeing the world/main.ts
@@ -0,0 +1,13 @@
+let favourite_place : string [] = ['Dubai','Canada','islamabad','bhawalpur','lahore']
+
+console.log("original   " + favourite_place );
+
+console.log("copy   " + [...favourite_place].sort() )
+
+console.log("original   " + favourite_place );
+
+console.log("copy  " + [...favourite_place].sort().reverse())
+
+console.log("original " + favourite_place );
+
+console.log("copy  " + [...favourite_place].sort())
