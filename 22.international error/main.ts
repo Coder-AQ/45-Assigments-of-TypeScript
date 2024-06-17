edefc8824df859cfd50fea02e82dece5dfda91ad
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:51:57 +0500

    Add files via upload

---
+++ 22.international error/main.ts
@@ -0,0 +1,7 @@
+let array : (string|number) [] = ['hadi','humdan','alyan',1,2,3,'rashid']
+
+console.log(array[5])
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[4])
