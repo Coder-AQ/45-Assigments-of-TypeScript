59a9d2f5f3f09f8c346ec613730ffee0fd84e286
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 19:49:46 +0500

    Add files via upload

---
+++ 19.dinner guest/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const main_1 = require("../14.Guest List/main");
+console.log(`\n NUMBER OF INVITED GUESTS`);
+console.log(`we had invited ${main_1.guest_list.length}`);
---
+++ 19.dinner guest/main.ts
@@ -0,0 +1,3 @@
+import{ guest_list } from '../14.Guest List/main'
+console.log(`\n NUMBER OF INVITED GUESTS`)
+console.log(`we had invited ${guest_list.length}`)
