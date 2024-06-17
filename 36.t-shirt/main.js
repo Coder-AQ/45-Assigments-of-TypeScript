c44f91fec46dd5fcdbe4ace110c3b304d161919e
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:08:10 +0500

    Add files via upload

---
+++ 36.t-shirt/main.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("Creating a ".concat(size, " shirt with the message: \"").concat(message, "\""));
+}
+make_shirt('large', 'Hello, world!');
---
+++ 36.t-shirt/main.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+  }
+  
+  make_shirt('large', 'Hello, world!');
