5efc2039dbda71ca9b543dac86949b3f755cdaa8
ANIQA <166115761+Coder-AQ@users.noreply.github.com>
2024-06-17 20:12:17 +0500

    Add files via upload

---
+++ 41.magician/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ["DAVID", "ZACK", "HARRY",];
+function show_magicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 41.magician/main.ts
@@ -0,0 +1,9 @@
+let magicians: string[] = ["DAVID", "ZACK", "HARRY", ];
+
+function show_magicians(magicians: string[]): void {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+
+show_magicians(magicians);
